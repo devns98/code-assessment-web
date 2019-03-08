a294e90f09070e7cf1a81dbe872a972228ac0dd6
Devanshu Agrawal <devns1998@gmail.com>
2019-03-07 19:02:46 -0500

    Added Remove Functionality and hooked up product api

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,11 +1,15 @@
 /**
  * Mocking client-server processing
  */
-import _products from './products.json'
+// import _products from './products.json'
+import $ from "jquery";
 
 const TIMEOUT = 100
 
 export default {
-  getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
+  getProducts: (cb, timeout) =>
+    $.getJSON("http://tech.work.co/shopping-cart/products.json", data => {
+      cb(data);
+    }),
   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
 }
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { addToCart } from '../actions'
+import { addToCart, removeFromCart } from '../actions'
 import { getVisibleProducts } from '../reducers/products'
 import ProductItem from '../components/ProductItem'
 import ProductsList from '../components/ProductsList'
 
-const ProductsContainer = ({ products, addToCart }) => (
+const ProductsContainer = ({ products, addToCart, removeFromCart }) => (
   <ProductsList title="Products">
     {products.map(product =>
       <ProductItem
         key={product.id}
         product={product}
-        onAddToCartClicked={() => addToCart(product.id)} />
+        onAddToCartClicked={() => addToCart(product.id)}
+        onRemoveFromCartClicked={() => removeFromCart(product.id)} />
     )}
   </ProductsList>
 )
@@ -24,7 +25,8 @@ ProductsContainer.propTypes = {
     price: PropTypes.number.isRequired,
     inventory: PropTypes.number.isRequired
   })).isRequired,
-  addToCart: PropTypes.func.isRequired
+  addToCart: PropTypes.func.isRequired,
+  removeFromCart: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
@@ -33,5 +35,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { addToCart }
+  { addToCart, removeFromCart }
 )(ProductsContainer)
